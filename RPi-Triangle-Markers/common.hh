b958c79b383e22d86704758e761572c1c5d39202
truposky <weselguti@gmail.com>
2021-12-15 12:06:07 +0100

    modificado

--- RPi-Triangle-Markers/common.hh
+++ RPi-Triangle-Markers/common.hh
@@ -26,7 +26,7 @@
 
 
 const int PORT = 4242;
-const int MAXDATASIZE = 256; //numero de bytes que se pueden recibir
+const int MAXDATASIZE =256; //numero de bytes que se pueden recibir
 const int HEADER_LEN = sizeof(unsigned short)*3;
 const int MAXROBOTS = 4;
 const char IP_SERVER[] = "192.168.1.2";
@@ -44,8 +44,7 @@ struct appdata{
         unsigned short id; //identificador
         unsigned short op; //codigo de operacion
         unsigned short len;                       /* longitud de datos */
-        char
-         data [MAXDATASIZE-HEADER_LEN];//datos
+        char data [MAXDATASIZE-HEADER_LEN];//datos
         //nota¡ actualizar char data a string o puntero para que sea mas versatil.
 
 
